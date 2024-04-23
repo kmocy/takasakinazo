23622646eb0f0fbba9d4b961a14f9b2f4f004146
kmocy <142728527+kmocy@users.noreply.github.com>
2024-04-23 21:08:56 +0900

    second commit
    second commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(function () {
+    $(".button_1").click(function () {
+        $(".nazo_img_1").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_2").click(function () {
+        $(".nazo_img_2").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_3").click(function () {
+        $(".nazo_img_3").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_4").click(function () {
+        $(".nazo_img_4").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_5").click(function () {
+        $(".nazo_img_5").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_6").click(function () {
+        $(".nazo_img_6").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_7").click(function () {
+        $(".nazo_img_7").slideToggle("");
+    });
+});
+$(function () {
+    $(".button_8").click(function () {
+        $(".nazo_img_8").slideToggle("");
+    });
+});
